69accac69e50d9a72e5d230299d6a746977d2261
rovaidkhan07 <rovaidkundi@gmail.com>
2025-07-14 15:41:37 -0700

    New task

---
+++ first.js
@@ -0,0 +1,8 @@
+const profile = {
+    name: "Rovaid khan",
+    age: 25,    
+    occupation: "Software Engineer",
+    skills: ["JavaScript", "Python", "React"],     
+    hobbies: ["Reading", "Gaming", "Traveling"],
+}
+console.log(profile);
